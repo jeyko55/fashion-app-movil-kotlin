f7926f56bfe21ef42b448e768df7941cd1cace89
Jacobo Palacio <jacobo.palacio@udea.edu.co>
2024-05-01 20:22:00 -0500

    Actualización Login

--- app/src/main/java/com/example/fashion_app_movil_kotlin/MainActivity.kt
+++ app/src/main/java/com/example/fashion_app_movil_kotlin/MainActivity.kt
@@ -10,6 +10,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.fashion_app_movil_kotlin.ui.login.ui.LoginScreen
 import com.example.fashion_app_movil_kotlin.ui.theme.FashionappmovilkotlinTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +23,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-
+                    LoginScreen()
                 }
             }
         }
--- app/src/main/java/com/example/fashion_app_movil_kotlin/ui/login/ui/LoginScreen.kt
+++ app/src/main/java/com/example/fashion_app_movil_kotlin/ui/login/ui/LoginScreen.kt
@@ -1,2 +1,107 @@
 package com.example.fashion_app_movil_kotlin.ui.login.ui
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.fashion_app_movil_kotlin.R
+
+@Composable
+fun LoginScreen(){
+    Box(
+        Modifier
+            .fillMaxSize()
+            .padding(16.dp)) {
+        Login(Modifier.align(Alignment.Center))
+    }
+}
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun PasswordField() {
+    TextField(value = "", onValueChange = {},
+        placeholder = { Text(text = "Contraseña")},
+        modifier = Modifier.fillMaxWidth(),
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
+        singleLine = true,
+        maxLines = 1,
+        colors = TextFieldDefaults.textFieldColors(
+            focusedIndicatorColor = Color.Transparent,
+            unfocusedIndicatorColor = Color.Transparent
+        )
+    )
+}
+
+@Composable
+fun Login(modifier: Modifier) {
+    Column(modifier = modifier) {
+        HeaderImage(Modifier.align(Alignment.CenterHorizontally))
+        Spacer(modifier = Modifier.padding(16.dp))
+        EmailField()
+        Spacer(modifier = Modifier.padding(4.dp))
+        PasswordField()
+        Spacer(modifier = Modifier.padding(8.dp))
+        ForgotPassword(Modifier.align(Alignment.End))
+    }
+}
+
+@Composable
+fun ForgotPassword(modifier: Modifier) {
+    Text(
+        text = "¿Olvidaste la contraseña?",
+        modifier = Modifier.clickable{ },
+        fontSize = 12.sp,
+        fontWeight = FontWeight.Bold,
+        color = Color(0xFFFB9600)
+    )
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun EmailField() {
+    TextField(
+        value = "",
+        onValueChange = {},
+        modifier = Modifier.fillMaxWidth(),
+        placeholder = {Text(text = "Email")},
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Email),
+        singleLine = true,
+        maxLines = 1,
+        colors = TextFieldDefaults.textFieldColors(
+            focusedIndicatorColor = Color.Transparent,
+            unfocusedIndicatorColor = Color.Transparent
+        )
+    // Corregir el uso de esta librería para los colores del textField en API 24
+
+    )
+}
+
+@Composable
+fun HeaderImage(modifier: Modifier) {
+    Image(
+        painter = painterResource(id = R.drawable.logo_fashionapp),
+        contentDescription = "Header",
+        modifier = Modifier
+    )
+}
