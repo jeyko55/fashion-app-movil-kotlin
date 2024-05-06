5ab02019872f60d0ce487154286f2095f35146b5
VanessaTocasuche <vanessa.tocasuche@udea.edu.co>
2024-05-05 21:05:17 -0500

    Formulario login sencillo

--- app/src/main/java/com/example/fashion_app_movil_kotlin/ui/login/ui/LoginScreen.kt
+++ app/src/main/java/com/example/fashion_app_movil_kotlin/ui/login/ui/LoginScreen.kt
@@ -7,40 +7,48 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
+import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.fashion_app_movil_kotlin.R
+import kotlinx.coroutines.launch
 
 @Composable
-fun LoginScreen(){
+fun LoginScreen(viewModel: LoginViewModel) {
     Box(
         Modifier
             .fillMaxSize()
-            .padding(16.dp)) {
-        Login(Modifier.align(Alignment.Center))
+            .padding(16.dp)
+    ) {
+        Login(Modifier.align(Alignment.Center), viewModel)
     }
 }
 
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun PasswordField() {
-    TextField(value = "", onValueChange = {},
-        placeholder = { Text(text = "Contraseña")},
+fun PasswordField(password: String, onTextFieldChanged: (String) -> Unit) {
+    TextField(
+        value = password, onValueChange = { onTextFieldChanged(it) },
+        placeholder = { Text(text = "Contraseña") },
         modifier = Modifier.fillMaxWidth(),
         keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
         singleLine = true,
@@ -53,15 +61,55 @@ fun PasswordField() {
 }
 
 @Composable
-fun Login(modifier: Modifier) {
-    Column(modifier = modifier) {
-        HeaderImage(Modifier.align(Alignment.CenterHorizontally))
-        Spacer(modifier = Modifier.padding(16.dp))
-        EmailField()
-        Spacer(modifier = Modifier.padding(4.dp))
-        PasswordField()
-        Spacer(modifier = Modifier.padding(8.dp))
-        ForgotPassword(Modifier.align(Alignment.End))
+fun Login(modifier: Modifier, viewModel: LoginViewModel) {
+
+    val email: String by viewModel.email.observeAsState(initial = "")
+    val password: String by viewModel.password.observeAsState(initial = "")
+    val loginEnable: Boolean by viewModel.loginEnable.observeAsState(initial = false)
+    val isLoading:Boolean by viewModel.isLoading.observeAsState(initial = false)
+    val coroutineScope = rememberCoroutineScope()
+
+    if (isLoading){
+        Box(Modifier.fillMaxSize())
+        {
+            CircularProgressIndicator(Modifier.align(Alignment.Center))
+        }
+    } else {
+        Column(modifier = modifier) {
+            HeaderImage(Modifier.align(Alignment.CenterHorizontally))
+            Spacer(modifier = Modifier.padding(16.dp))
+            EmailField(email) { viewModel.onLoginChanged(it, password) }
+            Spacer(modifier = Modifier.padding(4.dp))
+            PasswordField(password) { viewModel.onLoginChanged(email, it) }
+            Spacer(modifier = Modifier.padding(8.dp))
+            ForgotPassword(Modifier.align(Alignment.End))
+            Spacer(modifier = Modifier.padding(16.dp))
+            LoginButton(loginEnable) {
+                coroutineScope.launch {
+                    viewModel.onLoginSelected()
+                }
+            }
+
+        }
+    }
+
+
+}
+
+@Composable
+fun LoginButton(loginEnable: Boolean, onLoginSelected: () -> Unit) {
+    Button(
+        onClick = { onLoginSelected() },
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(48.dp),
+        colors = ButtonDefaults.buttonColors(
+            disabledContentColor = Color.White,
+            contentColor = Color.White,
+            containerColor = Color(0xFFFF4303),
+        ), enabled = loginEnable
+    ) {
+        Text(text = "Iniciar sesion")
     }
 }
 
@@ -69,7 +117,7 @@ fun Login(modifier: Modifier) {
 fun ForgotPassword(modifier: Modifier) {
     Text(
         text = "¿Olvidaste la contraseña?",
-        modifier = Modifier.clickable{ },
+        modifier = modifier.clickable { },
         fontSize = 12.sp,
         fontWeight = FontWeight.Bold,
         color = Color(0xFFFB9600)
@@ -77,14 +125,13 @@ fun ForgotPassword(modifier: Modifier) {
 }
 
 @OptIn(ExperimentalMaterial3Api::class)
-@Preview(showBackground = true, showSystemUi = true)
 @Composable
-fun EmailField() {
+fun EmailField(email: String, onTextFieldChanged: (String) -> Unit) {
     TextField(
-        value = "",
-        onValueChange = {},
+        value = email,
+        onValueChange = { onTextFieldChanged(it) },
         modifier = Modifier.fillMaxWidth(),
-        placeholder = {Text(text = "Email")},
+        placeholder = { Text(text = "Email") },
         keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Email),
         singleLine = true,
         maxLines = 1,
@@ -92,7 +139,7 @@ fun EmailField() {
             focusedIndicatorColor = Color.Transparent,
             unfocusedIndicatorColor = Color.Transparent
         )
-    // Corregir el uso de esta librería para los colores del textField en API 24
+        // Corregir el uso de esta librería para los colores del textField en API 24
 
     )
 }
